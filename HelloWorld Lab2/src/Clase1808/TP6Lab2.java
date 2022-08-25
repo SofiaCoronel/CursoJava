c719e08812b03d819057e14fbd9e2d6d94c20b1c
SofiaCoronel <sofia.coronel.2001@gmail.com>
2022-08-24 22:08:09 -0300

    Clase 18.08

--- HelloWorld Lab2/src/Clase1808/TP6Lab2.java
+++ HelloWorld Lab2/src/Clase1808/TP6Lab2.java
@@ -1,4 +1,25 @@
 package Clase1808;
 
 public class TP6Lab2 {
+    public static void main(String[] args){
+        //Math.
+
+        //FUNCIONES TRIGONOMETRICAS HABITUALES
+        System.out.println(Math.sin(0.5));
+        System.out.println(Math.cos(1.0));
+        System.out.println(Math.tan(1.0));
+        System.out.println(Math.atan(1.0));
+        System.out.println(Math.atan2(0.5, 1.5));
+        System.out.println();
+
+        //FUNCION EXPONENCIAL Y SU INVERSA
+        System.out.println(Math.exp(0.5));
+        System.out.println(Math.log(1.0));
+        System.out.println();
+
+        //CONSTANTES
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+    }
 }
