398b1f164a8b01c183c65586df22909577d1f852
SofiaCoronel <sofia.coronel.2001@gmail.com>
2022-09-05 20:02:36 -0300

    CursoDeJava

--- CursoJava Lab2/src/Clase2508/UsoCuenta.java
+++ CursoJava Lab2/src/Clase2508/UsoCuenta.java
@@ -1,126 +1,73 @@
 package Clase2508;
 
-import javafx.util.Pair;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
 public class UsoCuenta {
+    public UsoCuenta() {
+    }
+
     public static void main(String[] args) {
+        Scanner nom = new Scanner(System.in);
 
-        System.out.println("Cuenta Bancaria 1");
-        Pair<String, Double> datos = pedirDatos();
-        CuentaCorriente cuenta1 = new CuentaCorriente(datos.getKey(), datos.getValue());
+        System.out.println("Ingrese nombre titular Cuenta 1:");
+        String nombre = nom.next();
 
-        System.out.println("Cuenta Bancaria 2");
-        datos = pedirDatos();
-        CuentaCorriente cuenta2 = new CuentaCorriente(datos.getKey(), datos.getValue());
+        CuentaCorriente Cuenta1 = new CuentaCorriente(nombre, 1000.0D);
+        System.out.println("Ingrese nombre titular Cuenta 2:");
+        nombre = nom.next();
 
-        Double importe = pedirImporteUsuario();
-        CuentaCorriente.tranferencia(cuenta1,cuenta2,importe);
+        CuentaCorriente Cuenta2 = new CuentaCorriente(nombre, 1000.0D);
+        Cuenta1.ingreso(4000.0D);
 
-        System.out.println("Datos de la Cuenta 1");
-        List<String> datosCuenta;
-        datosCuenta = cuenta1.getDatosGeneralesCC();
-        System.out.println("Titular: "+ datosCuenta.get(0));
-        System.out.println("N° de Cuenta: "+ datosCuenta.get(1));
-        System.out.println("Saldo: "+ datosCuenta.get(2));
+        System.out.println("Antes de la transferencia:");
+        System.out.println("Cuenta 1:");
+        System.out.println(Cuenta1.toString());
+        System.out.println("Cuenta 2:");
+        System.out.println(Cuenta2.toString());
 
-        System.out.println("Datos de la Cuenta 2");
-        datosCuenta = cuenta2.getDatosGeneralesCC();
-        System.out.println("Titular: "+ datosCuenta.get(0));
-        System.out.println("N° de Cuenta: "+ datosCuenta.get(1));
-        System.out.println("Saldo: "+ datosCuenta.get(2));
+        double transf = 2500.0D;
 
-
+        Cuenta1.transferir(Cuenta2, transf);
+        System.out.println("Despues de la transferencia:");
+        System.out.println("Cuenta 1:");
+        System.out.println(Cuenta1.toString());
+        System.out.println("Cuenta 2:");
+        System.out.println(Cuenta2.toString());
     }
-    public void pedirDatos(String, Double){
-        System.out.println("Ingrese nombre del titular: ");
-        Scanner entradaEscaner = new Scanner(System.in);
-        String titular = entradaEscaner.nextLine();
-        Double saldo = 0.0;
-        System.out.println("Ingrese el saldo inicial: ");
-
-        while (!entradaEscaner.hasNextDouble()){
-            System.out.println("Ingrese un saldo correcto.\n"+"Vuelva a intentarlo (Solo numeros decimales): ");
-            entradaEscaner.nextLine();
-        }
-        saldo = entradaEscaner.nextDouble();
-        return new Pair<>(titular, saldo);
-    }
-
-
-    public static Double pedirImporteUsuario() {
-        //Pedimos el importe al usuario por consola
-        Scanner entradaEscaner = new Scanner (System.in); //Creamos de un objeto Scanner
-        Double importe = 0.0;
-        System.out.print ("\nIntroduzca el importe a transferir entre cuentas: ");
-        //Comprueba que sólo se introduzcan números válidos (Double)
-        while(!entradaEscaner.hasNextDouble())
-        {
-            System.out.print("No ha introducido un importe correcto.\n" +
-                    "Vuelva a intentarlo (sólo números y decimales): ");
-            entradaEscaner.nextLine();
-        }
-        importe = entradaEscaner.nextDouble();
-        entradaEscaner.close();
-        return importe;
-    }
-
 }
 
-class CuentaCorriente{
-    private String nombreTitular;
-    private Double saldo;
+class CuentaCorriente {
     private long numeroCuenta;
+    private String nombreTitular;
+    private double saldo;
 
-    public CuentaCorriente(String nombreTitular, Double saldo) {
+    public CuentaCorriente(String nombreTitular, double saldo) {
+        Random ran = new Random();
+        this.numeroCuenta = ran.nextLong(1000000L);
         this.nombreTitular = nombreTitular;
         this.saldo = saldo;
-        Random numCuentaAleat = new Random();
-        this.numeroCuenta = Math.abs(numCuentaAleat.nextLong());
     }
 
-    //SETTERS
-    public void setIngreso(Double ingreso) {
-        if (ingreso <= 0){
-            System.out.println("El ingreso debe ser mayor a $0");
-        } else {
-            saldo = saldo + ingreso;
-        }
+    public double getSaldo() {
+        return this.saldo;
     }
 
-    public void  setReintegro(Double reintegro){
-        if (reintegro <= 0){
-            System.out.println("El reintegro debe ser mayor a $0");
-        }else {
-            saldo = saldo - reintegro;
-        }
+    public String toString() {
+        return "CuentaCorriente: NumeroCuenta =" + this.numeroCuenta + ", nombre del Titular ='" + this.nombreTitular + "', saldo =" + this.saldo + ".";
     }
 
-    //GETTERS
-    public Double getSaldo(){
-        return saldo;
+    public void ingreso(double importe) {
+        this.saldo += importe;
     }
 
-    public List<String> getDatosGeneralesCC(){
-        List<String> datosCuentaC = new ArrayList<>();
-        datosCuentaC.add(nombreTitular);
-        datosCuentaC.add(Long.toString(numeroCuenta));
-
-        DecimalFormat saldoRedondeado = new DecimalFormat("###.##");
-        datosCuentaC.add(saldoRedondeado.format(saldo));
-
-        return datosCuentaC;
+    public void reintegro(double importe) {
+        this.saldo -= importe;
     }
 
-    //METODO DE TRANFERENCIA
-    public static void tranferencia(CuentaCorriente cuenta1, CuentaCorriente cuenta2, Double importeTranferencia){
-        //Double importeTranferencia = 2500;
-        cuenta1.saldo = cuenta1.saldo - importeTranferencia;
-        cuenta2.saldo = cuenta2.saldo + importeTranferencia;
+    public void transferir(CuentaCorriente cuenta, double importe) {
+        cuenta.ingreso(importe);
+        this.reintegro(importe);
     }
-
 }
+
